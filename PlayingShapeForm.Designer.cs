2b2098a73ec7c5c5666755ccf2a574ec3eaa1a7d
Snamik <49165134+Snamik@users.noreply.github.com>
2023-05-15 21:50:25 +0500

    Add form playing shape

---
+++ PlayingShapeForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ChessWF
+{
+    partial class PlayingShapeForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            GridShape = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)GridShape).BeginInit();
+            SuspendLayout();
+            // 
+            // GridShape
+            // 
+            GridShape.AllowUserToAddRows = false;
+            GridShape.AllowUserToDeleteRows = false;
+            GridShape.AllowUserToOrderColumns = true;
+            GridShape.AllowUserToResizeColumns = false;
+            GridShape.AllowUserToResizeRows = false;
+            GridShape.BackgroundColor = SystemColors.Control;
+            GridShape.CellBorderStyle = DataGridViewCellBorderStyle.Sunken;
+            GridShape.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            GridShape.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = Color.RosyBrown;
+            dataGridViewCellStyle1.Font = new Font("Palatino Linotype", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.MenuHighlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.False;
+            GridShape.DefaultCellStyle = dataGridViewCellStyle1;
+            GridShape.Dock = DockStyle.Fill;
+            GridShape.EditMode = DataGridViewEditMode.EditProgrammatically;
+            GridShape.Location = new Point(5, 5);
+            GridShape.Margin = new Padding(0);
+            GridShape.MultiSelect = false;
+            GridShape.Name = "GridShape";
+            GridShape.RowHeadersVisible = false;
+            GridShape.RowTemplate.Height = 25;
+            GridShape.Size = new Size(934, 751);
+            GridShape.TabIndex = 0;
+            // 
+            // PlayingShapeForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(944, 761);
+            Controls.Add(GridShape);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            HelpButton = true;
+            Name = "PlayingShapeForm";
+            Padding = new Padding(5);
+            Text = "PlayingShapeForm";
+            Load += Form_Load;
+            ((System.ComponentModel.ISupportInitialize)GridShape).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView GridShape;
+    }
+}
---
+++ PlayingShapeForm.cs
@@ -0,0 +1,99 @@
+
+namespace ChessWF
+{
+    public partial class PlayingShapeForm : Form
+    {
+        private const int ColumnCount = 10;
+        private const int RowCount = 10;
+        private const int SizeInfoFied = 25;
+        private const string MakerFieldText = "abcdefgh";
+        private const string MakerFieldNumber = "87654321";
+
+        public PlayingShapeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SetSizeGrid()
+        {
+            var rowHeight = (GridShape.Size.Height - SizeInfoFied * 2) / (RowCount - 2);
+            var rowWeight = (GridShape.Size.Width - SizeInfoFied * 2) / (ColumnCount - 2);
+
+            GridShape.Columns[0].Width = SizeInfoFied;
+            GridShape.Columns[ColumnCount - 1].Width = SizeInfoFied;
+
+            GridShape.Rows[0].Height = SizeInfoFied;
+            GridShape.Rows[RowCount - 1].Height = SizeInfoFied;
+
+            for (var i = 1; i < ColumnCount - 1; i++)
+            {
+                GridShape.Columns[i].Width = rowWeight;
+            }
+
+            for (var i = 1; i < RowCount - 1; i++)
+            {
+                GridShape.Rows[i].Height = rowHeight;
+            }
+        }
+
+        private void SetColor(Color bColor, Color wColor, bool inverse = false)
+        {
+            var cellStyleBlack = new DataGridViewCellStyle
+            {
+                BackColor = inverse ? wColor :  bColor
+
+            };
+            var cellStyleWhite = new DataGridViewCellStyle
+            {
+                BackColor = inverse ? bColor : wColor
+            };
+
+            for (var x = 1; x < ColumnCount - 1; x++)
+            {
+                var module = x % 2 == 0 ? 1 : 0;
+
+                for (var y = 1; y < RowCount - 1; y++)
+                {
+                    GridShape[x, y].Style = y % 2 == module ? cellStyleBlack : cellStyleWhite;
+                }
+            }
+        }
+
+        private void SetSymbols(bool inverse = false)
+        {
+            for (var x = 1; x < RowCount - 1; x++)
+            {
+                var index = inverse ? x : x - 1;
+
+                GridShape[x, 0].Value = MakerFieldText[index];
+                GridShape[x, RowCount - 1].Value = MakerFieldText[index];
+
+                GridShape[0, x].Value = MakerFieldNumber[index];
+                GridShape[ColumnCount - 1, x].Value = MakerFieldNumber[index];
+            }
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            GridShape.ScrollBars = ScrollBars.None;
+
+            for (var i = 0; i < ColumnCount; i++)
+            {
+                GridShape.Columns.Add(new DataGridViewColumn(cellTemplate: new DataGridViewTextBoxCell()));
+            }
+
+            for (var i = 0; i < RowCount; i++)
+            {
+                GridShape.Rows.Add();
+            }
+
+            GridShape.DefaultCellStyle.SelectionBackColor = Color.FromArgb(254, Color.Aqua);
+            GridShape.DefaultCellStyle.SelectionForeColor = Color.FromArgb(254, Color.Aqua);
+
+
+            SetSizeGrid();
+            SetColor(Color.DarkGoldenrod, Color.Bisque);
+            SetSymbols();
+        }
+    }
+}
